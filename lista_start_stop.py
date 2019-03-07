d1f66fd2973852c8b4041c6fda89c94598eaee52
miblazej <michalblazej@gmail.com>
2019-03-07 18:35:29 +0100

    7.2 wariant 1

--- lista_start_stop.py
+++ lista_start_stop.py
@@ -20,6 +20,16 @@ def odwrocSubListe(glowa, start, stop):
         sublista_iter.next, temp.next, start_zmiany.next = (temp.next, start_zmiany.next, temp)
     return glowa.next
 
+def odwrocListe(glowa):
+    pre = glowa
+    glowa = glowa.next
+    pre.next = None
+    while(1):
+        pre, glowa.next, glowa = (glowa, pre, glowa.next)
+        if (glowa.next is None):
+            break
+    glowa.next = pre
+    return glowa
 
 L1 = ListNode(1)
 last = L1
@@ -27,3 +37,6 @@ for i in range(2, 10, 2):
     L2 = ListNode(i)
     add_end(last, L2)
     last = L2
+
+a = odwrocListe(L1)
+print(a)
